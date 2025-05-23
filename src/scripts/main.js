38e0cc3c94dd759723a5ba8f7e212e5c50a0d819
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-23 20:03:25 +0300

    solution for fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderSize = parseInt(getComputedStyle(spider).height, 10);
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10);
+  const wallRect = wall.getBoundingClientRect();
+
+  if (e.target.closest('.wall')) {
+    const vertShift = e.clientY - wallRect.top - wallBorder - spiderSize / 2;
+    const horizShift = e.clientX - wallRect.left - wallBorder - spiderSize / 2;
+
+    if (vertShift < wall.offsetLeft && horizShift < wall.offsetTop) {
+      spider.style.top = vertShift + 'px';
+      spider.style.left = horizShift + 'px';
+    }
+  }
 });
